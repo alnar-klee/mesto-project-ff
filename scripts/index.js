38862567f8f9ab132c010bd0a3c483a45717e21f
Alunaru <alnar.klee@gmail.com>
2025-05-22 11:43:54 +0300

    1ver

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,28 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector('#card-template').content;
+
+const placesList = document.querySelector('.places__list');
+
+function createCard(element) {
+  const card = cardTemplate.cloneNode(true);
+
+  card.querySelector('.card__image').src = element.link;
+  card.querySelector('.card__image').alt = element.name;
+  card.querySelector('.card__title').textContent = element.name;
+
+  card.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+
+  return card;
+}
+
+function deleteCard(evt) {
+  evt.target.closest('.places__item').remove();  
+}
+
+initialCards.forEach(function (element) {
+  const card = createCard(element);
+  placesList.append(card);
+})
